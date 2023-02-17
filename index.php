fece6827f122218c13f0b5d10ec9808b9af578fb
jaycob14 <54973944+jaycob14@users.noreply.github.com>
2023-02-17 18:30:07 +0800

    Third Commit Basics

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
-<?php
-    echo 'Hello World';
-    echo "Test 1";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Primer - Index</title>
+</head>
+<body>
+    <!-- Basic HTML -->
+    <h1>Hello HTML World</h1>
+
+    <?php
+
+        echo 'Hello PHP';
+
+    ?>
+
+    <?php 
+
+        $name = 'Jay';
+        $age = 23;
+        
+        echo '<br>';
+        echo $name;
+
+        echo '<h1> My Name is: ' .$name. '</h1>';
+        
+        echo '<h1> My Age is: ' .$age. '</h1>';
+
+        echo "<h1> My Name is: $name </h1>"
+
+    ?>
+</body>
+</html>
