f08b1075de0902ef62041f45fa6a2ce7bb703505
jaycob14 <54973944+jaycob14@users.noreply.github.com>
2023-02-27 17:28:50 +0800

    nav bar added

--- includes/header.php
+++ includes/header.php
@@ -4,13 +4,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet "href="assets/hover.css">
+
     <title>PHP Primer - <?php echo $title ?></title>
 </head>
 <body>
     
-    <h2 style = "color:skyblue">Choices</h2>
+    <div class="container">
 
-        <ul>
+    <h2 style = "color:skyblue" class= "text-center">Choices</h2>
+
+
+
+<nav class="navbar navbar-expand-sm navbar-dark">
+    <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+            <li class="nav-item active"> <a href="ifstatement.php" class="nav-link"> If Statement Page </a> </li>
+            <li class="nav-item active"> <a href="switchstatement.php" class="nav-link"> Switch Statement Page </a> </li>
+            <li class="nav-item active"> <a href="forloop.php" class="nav-link"> For Loop Page </a> </li>
+            <li class="nav-item active"> <a href="whiledowhile.php" class="nav-link"> While Do While </a> </li>
+            <li class="nav-item active"> <a href="simplearray.php" class="nav-link"> Simple Arrays</a> </li>
+            <li class="nav-item active"> <a href="stringmanipulation.php" class="nav-link"> String Manipulation </a> </li>
+            <li class="nav-item active"> <a href="dateandtime.php" class="nav-link"> Date and Time Manipulation </a> </li>
+            <li class="nav-item active"> <a href="functions.php" class="nav-link"> User Defined Functions </a> </li>
+            <li class="nav-item dropdown active">
+            <a class="dropdown-toggle nav-link" data-bs-toggle="dropdown" aria-expanded="false">
+                Extra
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+            </li>
+
+        </ul>
+    </div>
+</nav>
+<!-- Nav -->
+    <!-- <ul class="nav nav-pills nav-fill justify-content-center">
+
+        <li class="nav-item"></li>
+            <a href="ifstatement.php" class="nav-link">If Statement Page</a>
+        <li class="nav-item"></li>
+            <a href="switchstatement.php" class="nav-link active">Switch Statement Page</a>
+        <li class="nav-item"></li>
+            <a href="forloop.php" class="nav-link active">For Loop Page</a>
+        <li class="nav-item"></li>
+            <a href="whiledowhile.php" class="nav-link active">While Do While</a>
+        <li class="nav-item"></li>
+            <a href="simplearray.php" class="nav-link active">Simple Arrays</a>
+        <li class="nav-item"></li>
+            <a href="stringmanipulation.php" class="nav-link active">String Manipulation</a>
+        <li class="nav-item"></li>
+            <a href="dateandtime.php" class="nav-link active">Date and Time Manipulation</a>
+        <li class="nav-item"></li>
+            <a href="functions.php" class="nav-link active">User Defined Functions</a>
+
+
+
+    </ul> -->
+        <!-- <ul>
 
             <li><a href="ifstatement.php">If Statement Page</a></li>
             <li><a href="switchstatement.php">Switch Statement Page</a></li>
@@ -21,4 +81,4 @@
             <li><a href="dateandtime.php">Date and Time Manipulation</a></li>
             <li><a href="functions.php">User Defined Functions</a></li>
 
-        </ul>
+        </ul> -->
