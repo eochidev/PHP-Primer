a54001b76f15a5bf7a7cba0232eb446cd27f57f4
jaycob14 <54973944+jaycob14@users.noreply.github.com>
2023-02-27 17:26:37 +0800

    navigation bar added

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,9 @@
 <?php echo "<p>PHP MASTERCLASS - JAY MAGTALAS</p>" . date("MY")?>
 
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" 
+integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
 </body>
 </html>
